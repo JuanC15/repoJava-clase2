889e78168d562f33c2aa213973a27b1232c8f201
Juan <j.carlosarocav@gmail.com>
2025-10-16 21:36:52 -0500

    funcionalidad agregada

--- src/App.java
+++ src/App.java
@@ -2,5 +2,6 @@ public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hola mundo Dev Senior");
         System.out.println("Modificacion 1");
+        System.out.println("Modificacion 2");
     }
 }
